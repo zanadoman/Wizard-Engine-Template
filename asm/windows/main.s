a1d393886c429e136256149f6978f6b5a32a5995
zanadoman <domanseven@gmail.com>
2024-02-20 14:27:40 -0800

    .

--- asm/windows/main.s
+++ asm/windows/main.s
@@ -1,10 +1,7 @@
 	.file	"main.cpp"
 	.text
 	.def	__main;	.scl	2;	.type	32;	.endef
-	.section	.text.unlikely,"x"
-.LCOLDB3:
 	.section	.text.startup,"x"
-.LHOTB3:
 	.p2align 4
 	.globl	main
 	.def	main;	.scl	2;	.type	32;	.endef
@@ -40,15 +37,15 @@ main:
 	call	_ZN3wze6engineC1EPKcS2_tth
 .LEHE1:
 	movq	.LC0(%rip), %rax
-	pxor	%xmm3, %xmm3
+	leaq	272(%rsi), %rcx
 	xorl	%r8d, %r8d
 	xorl	%edx, %edx
 	movl	$50, 48(%rsp)
-	leaq	272(%rsi), %rcx
+	pxor	%xmm3, %xmm3
 	leaq	192(%rsi), %rbx
-	movq	%rax, 56(%rsp)
 	movl	$50, 40(%rsp)
 	movq	$0x000000000, 32(%rsp)
+	movq	%rax, 56(%rsp)
 .LEHB2:
 	call	_ZN3wze6engine6actors3NewEPvyddttd
 	leaq	8(%rax), %rcx
@@ -126,8 +123,8 @@ main:
 	call	_ZdlPvy
 	nop
 	movaps	80(%rsp), %xmm6
-	movaps	96(%rsp), %xmm7
 	xorl	%eax, %eax
+	movaps	96(%rsp), %xmm7
 	addq	$112, %rsp
 	popq	%rbx
 	popq	%rsi
@@ -186,11 +183,16 @@ main:
 	mulsd	%xmm7, %xmm2
 	subsd	%xmm2, %xmm1
 	call	_ZN3wze6engine6actors5actor4SetXEd
-.LEHE2:
 	jmp	.L6
 .L23:
 	movq	%rax, %rbx
-	jmp	.L22
+	movq	%rsi, %rcx
+	movl	$480, %edx
+	call	_ZdlPvy
+	movq	%rbx, %rcx
+	call	_Unwind_Resume
+	nop
+.LEHE2:
 	.def	__gxx_personality_seh0;	.scl	2;	.type	32;	.endef
 	.seh_handler	__gxx_personality_seh0, @unwind, @except
 	.seh_handlerdata
@@ -215,46 +217,6 @@ main:
 .LLSDACSE6889:
 	.section	.text.startup,"x"
 	.seh_endproc
-	.section	.text.unlikely,"x"
-	.def	main.cold;	.scl	3;	.type	32;	.endef
-	.seh_proc	main.cold
-	.seh_stackalloc	136
-	.seh_savereg	%rbx, 112
-	.seh_savereg	%rsi, 120
-	.seh_savereg	%rdi, 128
-	.seh_savexmm	%xmm6, 80
-	.seh_savexmm	%xmm7, 96
-	.seh_endprologue
-main.cold:
-.L22:
-	movq	%rsi, %rcx
-	movl	$480, %edx
-	call	_ZdlPvy
-	movq	%rbx, %rcx
-.LEHB3:
-	call	_Unwind_Resume
-	nop
-.LEHE3:
-	.seh_handler	__gxx_personality_seh0, @unwind, @except
-	.seh_handlerdata
-.LLSDAC6889:
-	.byte	0xff
-	.byte	0xff
-	.byte	0x1
-	.uleb128 .LLSDACSEC6889-.LLSDACSBC6889
-.LLSDACSBC6889:
-	.uleb128 .LEHB3-.LCOLDB3
-	.uleb128 .LEHE3-.LEHB3
-	.uleb128 0
-	.uleb128 0
-.LLSDACSEC6889:
-	.section	.text.unlikely,"x"
-	.section	.text.startup,"x"
-	.section	.text.unlikely,"x"
-	.seh_endproc
-.LCOLDE3:
-	.section	.text.startup,"x"
-.LHOTE3:
 	.section .rdata,"dr"
 	.align 8
 .LC0:
@@ -264,7 +226,7 @@ main.cold:
 .LC2:
 	.long	0
 	.long	1071644672
-	.ident	"GCC: (GNU) 13.1.0"
+	.ident	"GCC: (x86_64-posix-seh-rev1, Built by MinGW-Builds project) 13.2.0"
 	.def	_Znwy;	.scl	2;	.type	32;	.endef
 	.def	_ZN3wze6engineC1EPKcS2_tth;	.scl	2;	.type	32;	.endef
 	.def	_ZN3wze6engine6actors3NewEPvyddttd;	.scl	2;	.type	32;	.endef
